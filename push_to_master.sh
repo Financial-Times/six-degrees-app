6f44bf18ab417bb59fbe1be15a4c2cfcf8df5181
Tomasz Libich <tomasz.libich@ft.com>
2016-06-07 14:27:30 +0100

    Updating push script

--- push_to_master.sh
+++ push_to_master.sh
@@ -2,5 +2,6 @@ git config --global user.email "t.libich@gmail.com"
 git config --global user.name "tomaszlibich"
 
 gulp client-build-release
+git add .
 git commit -m "Version update"
 git push origin develop:master
