4aed475be4f84b9dcccfb2b462524953d4327357
Tomasz Libich <tomasz.libich@ft.com>
2016-06-07 16:22:58 +0100

    Updating deployment script with tagging

--- deployment/build_release.sh
+++ deployment/build_release.sh
@@ -13,3 +13,4 @@ git add .
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
 DESCRIPTION="Version update: $PACKAGE_VERSION"
 git commit -m "$DESCRIPTION"
+git tag -a $PACKAGE_VERSION -m "ver $PACKAGE_VERSION"
