781bf3553d5e4beb80c57f4c0b9ff630f25c3aef
Tomasz Libich <tomasz.libich@ft.com>
2016-06-08 12:46:50 +0100

    Adding v to git tag in build release script, push to origin

--- deployment/build_release.sh
+++ deployment/build_release.sh
@@ -13,5 +13,5 @@ git add .
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
 DESCRIPTION="Version update: $PACKAGE_VERSION"
 git commit -m "$DESCRIPTION"
-git tag -a $PACKAGE_VERSION -m "ver $PACKAGE_VERSION"
+git tag -a v$PACKAGE_VERSION -m "ver $PACKAGE_VERSION"
 git push origin v$PACKAGE_VERSION
